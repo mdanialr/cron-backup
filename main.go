3ec7d2da488cafaaa53d39ad9b43cac51168eacf
MDanial <mdanialrma@gmail.com>
2021-11-03 15:18:49 +0700

    ~ref: seperate flag-thing from main func

--- main.go
+++ main.go
@@ -9,14 +9,7 @@ import (
 )
 
 func main() {
-	flag.BoolVar(&helpers.TCond.IsTest, "test", false, "to test this app functioning properly")
-	flag.BoolVar(&helpers.TCond.IsDel, "d", false, "to delete all created dir after this test")
-	flag.BoolVar(&helpers.TCond.IsNoDB, "no-db", false, "to exclude database from this backup test")
-	flag.BoolVar(&helpers.TCond.IsNoAPP, "no-app", false, "to exclude app from this backup test")
-	flag.IntVar(&helpers.TCond.Sample, "sample", 1, "set number of samples for both app and database")
-	flag.IntVar(&helpers.TCond.Sapp, "sam-app", 1, "spesifically set number of samples for app")
-	flag.IntVar(&helpers.TCond.Sdb, "sam-db", 1, "spesifically set number of samples for database")
-	flag.Parse()
+	setupFlag()
 
 	// Run backup only if there is no test flag
 	if !helpers.TCond.IsTest {
@@ -32,4 +25,16 @@ func main() {
 		return
 	}
 	utils.RunTest()
+}
+
+// setupFlag initialize flag then parse it.
+func setupFlag() {
+	flag.BoolVar(&helpers.TCond.IsTest, "test", false, "to test this app functioning properly")
+	flag.BoolVar(&helpers.TCond.IsDel, "d", false, "to delete all created dir after this test")
+	flag.BoolVar(&helpers.TCond.IsNoDB, "no-db", false, "to exclude database from this backup test")
+	flag.BoolVar(&helpers.TCond.IsNoAPP, "no-app", false, "to exclude app from this backup test")
+	flag.IntVar(&helpers.TCond.Sample, "sample", 1, "set number of samples for both app and database")
+	flag.IntVar(&helpers.TCond.Sapp, "sam-app", 1, "spesifically set number of samples for app")
+	flag.IntVar(&helpers.TCond.Sdb, "sam-db", 1, "spesifically set number of samples for database")
+	flag.Parse()
 }
